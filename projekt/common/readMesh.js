8fb6f034e8a502d4efc164353ebd97fc9fe1f70d
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2022-11-24 13:22:06 +0100

    template files

--- projekt/common/readMesh.js
+++ projekt/common/readMesh.js
@@ -1,3 +1,36 @@
+async function readMeshAsync(filename, callback) {
+    let filenames = [filename];
+    let result = await new Promise((resolve) => {
+        assimpjs().then(
+            function(ajs) {
+                Promise.all (filenames.map ((file) => fetch (file))).then ((responses) => {
+                    return Promise.all (responses.map ((res) => res.arrayBuffer ()));
+                }).then ((arrayBuffers) => {
+                    // create new file list object, and add the filename
+                    let fileList = new ajs.FileList ();
+                    fileList.AddFile (filename, new Uint8Array (arrayBuffers[0]));
+
+                    let result = ajs.ConvertFileList (fileList, 'assjson');
+                    if (!result.IsSuccess () || result.FileCount () == 0) {
+                        console.log("Could not load mesh: " + filename);
+                        return null;
+                    }
+
+                    // get the result file, and convert to string
+                    let resultFile = result.GetFile (0);
+                    let jsonContent = new TextDecoder ().decode (resultFile.GetContent ());
+                    let resultJson = JSON.parse (jsonContent);
+
+                    // parse the result json
+                    resolve(callback(resultJson));
+                })
+            }
+        );
+    });
+
+    return result;
+}
+
 function readMesh(filename, callback) {
     assimpjs().then(
         function(ajs) {
